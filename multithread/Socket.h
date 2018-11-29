539923eb246efa99d9f86bfc9304b075c604a546
Chris Perlette <chrisperlette@gmail.com>
2018-11-28 19:15:40 -0700

    finished multithread, moving onto S1

---
+++ multithread/Socket.h
@@ -0,0 +1,51 @@
+// Definition of the Socket class
+
+#ifndef Socket_class
+#define Socket_class
+/*
+#ifndef MSG_NOSIGNAL
+#define MSG_NOSIGNAL 0x4000   /* Do not generate SIGPIPE */
+//#endif
+
+#include <string>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+const int MAXHOSTNAME = 200;
+const int MAXCONNECTIONS = 5;
+const int MAXRECV = 500;
+
+class Socket
+{
+public:
+   Socket();
+   virtual ~Socket();
+
+   // Server initialization
+   bool create();
+   bool bind (const int port);
+   bool listen() const;
+   bool accept (Socket&) const;
+
+   // Client initialization
+   bool connect (const std::string host, const int port);
+
+   // Data Transimission
+   bool send (const std::string) const;
+   int recv (std::string&) const;
+
+   bool is_valid() const {
+      return m_sock != -1; }
+
+private:
+   int m_sock;
+   sockaddr_in m_addr;
+};
+
+
+#endif
